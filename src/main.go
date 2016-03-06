3c8aa8d17014e113939576768939ab5156230060
Nikita Chudinov <imonory@yandex.ru>
2016-03-06 22:30:04 +0300

    Add some date handling functions

--- src/main.go
+++ src/main.go
@@ -8,4 +8,9 @@ import (
 func main() {
 	resp, _ := ruz.MakeRequest("npchudinov@edu.hse.ru", "2016.2.22", "2016.2.25")
 	fmt.Println(ruz.PrettifyResponse(resp))
+
+	date, _ := ruz.ParseDate("6.3.2016")
+	fmt.Println(date)
+	mon, sat := ruz.FindWeek(date)
+	fmt.Println(ruz.FormatDate(mon), ruz.FormatDate(sat))
 }
