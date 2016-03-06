c85eb53bb6c0da4ab9bf9c3783d9ae9bd12b6c6b
Nikita Chudinov <imonory@yandex.ru>
2016-03-06 22:57:29 +0300

    Add pretty schedule printing

--- src/main.go
+++ src/main.go
@@ -2,15 +2,10 @@ package main
 
 import (
 	"./ruz"
-	"fmt"
 )
 
 func main() {
 	resp, _ := ruz.MakeRequest("npchudinov@edu.hse.ru", "2016.2.22", "2016.2.25")
-	fmt.Println(ruz.PrettifyResponse(resp))
 
-	date, _ := ruz.ParseDate("6.3.2016")
-	fmt.Println(date)
-	mon, sat := ruz.FindWeek(date)
-	fmt.Println(ruz.FormatDate(mon), ruz.FormatDate(sat))
+	ruz.PrintWeek(resp)
 }
